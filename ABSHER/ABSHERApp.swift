21571367ccf6740473c186267017f85cd12cb4b2
Wiqaya <Wiqaya>
2025-11-24 15:58:09 +0300

    Initial Commit

---
+++ ABSHER/ABSHERApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABSHERApp.swift
+//  ABSHER
+//
+//  Created by Ilyas MacBook Pro on 03/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct ABSHERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ABSHERTests/ABSHERTests.swift
@@ -0,0 +1,17 @@
+//
+//  ABSHERTests.swift
+//  ABSHERTests
+//
+//  Created by Ilyas MacBook Pro on 03/06/1447 AH.
+//
+
+import Testing
+@testable import ABSHER
+
+struct ABSHERTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
